c3b5894edb859231ed32efdfccd1770f9de11468
jhelm3 <jakehelm36@gmail.com>
2020-03-10 16:32:33 -0600

    added virtual "creator"

--- server/models/Exercises.js
+++ server/models/Exercises.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const ObjId = mongoose.Schema.Types.ObjectId
 
-const Workout = new Schema(
+const Exercise = new Schema(
   {
     title: [{ type: String }],
     sets: {type: Number},
@@ -11,9 +11,17 @@ const Workout = new Schema(
     distance: {type: Number},
     time: {type: String},
     workoutId: { type: ObjId, ref: "Workout"},
+    creatorEmail: { type: String, required: true }
     // NOTE If you wish to add additional public properties for Workouts do so here
   },
-  { timestamps: true, toJSON: { virtuals: true } }
-);
+  { timestamps: true, toJSON: { virtuals: true } })
+
+  Exercise.virtual("creator",
+  {
+    localField: "creatorEmail",
+    ref: "Profile",
+    foreignField: "email",
+    justOne: true
+  })
 
 export default Workout;
--- server/models/Workout.js
+++ server/models/Workout.js
@@ -4,13 +4,20 @@ const ObjId = mongoose.Schema.Types.ObjectId
 
 const Workout = new Schema(
   {
-    title: [{ type: String, unique: true }],
-    userId: { type: ObjId, ref: "Profile"},
+    title: { type: String, unique: true },
+    creatorEmail: { type: String, required: true },
     exercises: [],
     type: { type: String }
     // NOTE If you wish to add additional public properties for Workouts do so here
   },
-  { timestamps: true, toJSON: { virtuals: true } }
-);
+  { timestamps: true, toJSON: { virtuals: true } })
+
+  Workout.virtual("creator",
+  {
+    localField: "creatorEmail",
+    ref: "Profile",
+    foreignField: "email",
+    justOne: true
+  })
 
 export default Workout;
