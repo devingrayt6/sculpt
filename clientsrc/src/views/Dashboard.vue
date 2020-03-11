4a776761e0ca16440ff1c03a77f2ff63c7f1e29c
devingrayt6 <devingrayt6@gmail.com>
2020-03-11 11:19:00 -0600

    dashboard styling

--- clientsrc/src/views/Dashboard.vue
+++ clientsrc/src/views/Dashboard.vue
@@ -1,22 +1,77 @@
 <template>
-  <div>
+  <div class="container-fluid">
     <div>
-      <h3 class="text-center mt-2">{{todaysDate}}</h3>
+      <h3 class="text-center my-3">{{dayOfWeek}} {{todaysDate}}</h3>
     </div>
     <div class="row">
-      <div offset-2 col-8>
+      <div class="offset-1 col-10 offset-1">
         <h2 class="overlay">Todays Workout</h2>
         <img
           class="todays-workout"
-          src="https://images.pexels.com/photos/841130/pexels-photo-841130.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          src="https://images.pexels.com/photos/841130/pexels-photo-841130.jpeg?auto=compress&cs=tinysrgb&dpr=1"
           alt
         />
       </div>
     </div>
+
+    <div class="calendar" id="calendar">
+      <p>Sun</p>
+      <p>Mon</p>
+      <p>Tue</p>
+      <p>Wed</p>
+      <p>Thu</p>
+      <p>Fri</p>
+      <p>Sat</p>
+    </div>
+
+    <div class="row">
+      <div class="stats offset-1 col-10 offset-1">
+        <div class="text-center">
+          <h3 class="mt-3">STATS:</h3>
+        </div>
+
+        <ul>
+          <li id="bench">
+            Bench
+            <i class="fas fa-chart-line"></i>
+          </li>
+          <li id="squat">
+            Squat
+            <i class="fas fa-chart-line"></i>
+          </li>
+          <li id="dead-lift">
+            Dead Lift
+            <i class="fas fa-chart-line"></i>
+          </li>
+          <li id="push-ups">
+            Push Ups
+            <i class="fas fa-chart-line"></i>
+          </li>
+          <li id="pull-ups">
+            Pull Ups
+            <i class="fas fa-chart-line"></i>
+          </li>
+          <li id="sit-ups">
+            Sit Ups
+            <i class="fas fa-chart-line"></i>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <footer></footer>
   </div>
 </template>
 
 <script>
+let weekDays = {
+  "0": "Sunday",
+  "1": "Monday",
+  "2": "Tuesday",
+  "3": "Wednesday",
+  "4": "Thursday",
+  "5": "Friday",
+  "6": "Saturday"
+};
 export default {
   name: "Dashboard",
   mounted: {},
@@ -30,7 +85,8 @@ export default {
         .replace(/-/g, "/")
         .split("/")
         .reverse()
-        .join("/")}`
+        .join("/")}`,
+      dayOfWeek: `${weekDays[new Date().getDay()]}`
     };
   }
 };
@@ -39,12 +95,45 @@ export default {
 <style scoped>
 .overlay {
   position: absolute;
-  background: rgba(0, 0, 0, 0.5);
+  /* background: rgba(0, 0, 0, 0.5); */
   width: 100%;
   height: 50;
   text-align: center;
   color: white;
+  margin-top: 8rem;
 }
 .todays-workout {
+  width: 100%;
+}
+.calendar {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  margin-top: 2.5rem;
+  margin-bottom: 1.5rem;
+}
+.calendar p {
+  color: rgb(124, 121, 121);
+}
+.stats {
+  background-color: rgb(0, 0, 0);
+  bottom: 0;
+  width: 100%;
+  height: 25rem;
+  color: white;
+}
+.stats h3 {
+  color: rgb(255, 255, 255);
+}
+.stats ul {
+  list-style-type: none;
+}
+.stats ul li {
+  font-size: 18px;
+  margin-top: 0.75rem;
+  margin-bottom: 0.75rem;
+}
+.stats ul i {
+  color: green;
 }
 </style>
