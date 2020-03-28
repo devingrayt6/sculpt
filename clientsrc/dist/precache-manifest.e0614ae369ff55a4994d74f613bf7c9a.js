a4e6a6f3ef8a27b9e2afdb2cd3fe014922a9e39b
devingrayt6 <devingrayt6@gmail.com>
2020-03-27 18:10:31 -0600

    second production

---
+++ clientsrc/dist/precache-manifest.e0614ae369ff55a4994d74f613bf7c9a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21eb4f4f229bee12b1039c0593491fce",
+    "url": "Boxing-gym.mp4"
+  },
+  {
+    "revision": "acb46eb7c414b126137bf805db8cdf75",
+    "url": "Sit-Up.jpg"
+  },
+  {
+    "revision": "426bb7f751b57328cd601d15cb6979c2",
+    "url": "arms.jpg"
+  },
+  {
+    "revision": "85f840c2d4e39b5d19af5586847724ba",
+    "url": "background1.jpg"
+  },
+  {
+    "revision": "913f7de84f7af04934e64b694c8e2385",
+    "url": "background2.jpg"
+  },
+  {
+    "revision": "8e453bcb275f05b1d92a6a2439a62888",
+    "url": "bench.jpg"
+  },
+  {
+    "revision": "4f0a31cf5307320234ad",
+    "url": "css/app.6de3396c.css"
+  },
+  {
+    "revision": "30b86b62590bdc3a01f10d25ea29899d",
+    "url": "deadlift.jpg"
+  },
+  {
+    "revision": "1266342cf8689702af34141063a2d3ea",
+    "url": "dumbbell.png"
+  },
+  {
+    "revision": "0d43c0bc4fcd8b09cd8ff3cbb0de8ebc",
+    "url": "img/restday.0d43c0bc.jpg"
+  },
+  {
+    "revision": "9e7cfc18cd27d09ace246e664226fc0e",
+    "url": "index.html"
+  },
+  {
+    "revision": "4f0a31cf5307320234ad",
+    "url": "js/app.0b2d04b5.js"
+  },
+  {
+    "revision": "2d6ea6ca832839ddaaba",
+    "url": "js/chunk-vendors.48d13351.js"
+  },
+  {
+    "revision": "b78ba39fbf8f554521b8afd3a30b0fed",
+    "url": "leg.jpg"
+  },
+  {
+    "revision": "545fe45dce1a4e173beb8bdfbabb1961",
+    "url": "logo.png"
+  },
+  {
+    "revision": "869faeafe52569fa46e16827c266ccd7",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "21eb4f4f229bee12b1039c0593491fce",
+    "url": "media/Boxing-gym.21eb4f4f.mp4"
+  },
+  {
+    "revision": "d3c1277341e2b9f1e87f2a69deb51ede",
+    "url": "pull up.jpg"
+  },
+  {
+    "revision": "217ed9e9127105f524926ebea0267e87",
+    "url": "pushup.jpg"
+  },
+  {
+    "revision": "5ecef5aa3305df7c9690730fbb90d1ac",
+    "url": "rest.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "c8e7c80a91e1982f47068a49242a3cc6",
+    "url": "run.jpg"
+  },
+  {
+    "revision": "80f7f346cdc867752e909ccb3c85b6a9",
+    "url": "shoulder.jpg"
+  },
+  {
+    "revision": "177d858c4da9842c27934bdf6a312551",
+    "url": "squat.jpg"
+  },
+  {
+    "revision": "5e27ca000e72d46bcc9e49b9b25eddea",
+    "url": "style.css"
+  }
+]);
