c8a487e2da5708eb14c75bffdf0b9aa35e990d0e
devingrayt6 <devingrayt6@gmail.com>
2020-03-27 21:54:59 -0600

    third times the charm?

--- clientsrc/vue.config.js
+++ clientsrc/vue.config.js
@@ -1,9 +1,8 @@
 const { GenerateSW } = require("workbox-webpack-plugin");
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "development" ? "/vuejs-pwa/":"",
-  outputDir,
-  configureWebpack: {
-    plugins: [new GenerateSW()]
-  }
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/production-sub-path/'
+    : '/',
+  outputDir: 'folderpath'
+}
