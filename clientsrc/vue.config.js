17956ea87aa16f410012a9f2da4ee009479050d8
devingrayt6 <devingrayt6@gmail.com>
2020-03-27 21:31:20 -0600

    Fixing outputDir

--- clientsrc/vue.config.js
+++ clientsrc/vue.config.js
@@ -1,8 +1,8 @@
 const { GenerateSW } = require("workbox-webpack-plugin");
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "development" ? "/vuejs-pwa/" : outputDir,
-
+  publicPath: process.env.NODE_ENV === "development" ? "/vuejs-pwa/":"",
+  outputDir,
   configureWebpack: {
     plugins: [new GenerateSW()]
   }
